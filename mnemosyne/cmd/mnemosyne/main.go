dfbd63b3f9b127e4836c675f9aaa40784e05ad76
Kevin Fang <hkr.fang@gmail.com>
2026-03-20 15:35:29 -0700

    use environment vars

--- mnemosyne/cmd/mnemosyne/main.go
+++ mnemosyne/cmd/mnemosyne/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strconv"
 	"syscall"
 
 	"github.com/kfang/mnemosyne/internal/importer"
@@ -14,34 +14,28 @@ import (
 )
 
 func main() {
-	importDir := flag.String("import", "", "directory to watch for new photos")
-	libraryDir := flag.String("library", "", "root directory of the photo library")
-	addr := flag.String("addr", ":8080", "web server listen address")
-	workers := flag.Int("workers", 4, "number of parallel import workers")
-	flag.Parse()
+	importDir := envRequired("MNEMOSYNE_IMPORT_DIR")
+	libraryDir := envRequired("MNEMOSYNE_LIBRARY_DIR")
+	addr := envDefault("MNEMOSYNE_ADDR", ":8080")
+	workers := envDefaultInt("MNEMOSYNE_WORKERS", 4)
 
-	if *importDir == "" || *libraryDir == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	absLibrary, err := filepath.Abs(*libraryDir)
+	absLibrary, err := filepath.Abs(libraryDir)
 	if err != nil {
 		log.Fatalf("failed to resolve library path: %v", err)
 	}
 
-	imp := importer.New(absLibrary, *workers)
-	w, err := watcher.New(*importDir, imp)
+	imp := importer.New(absLibrary, workers)
+	w, err := watcher.New(importDir, imp)
 	if err != nil {
 		log.Fatalf("failed to create watcher: %v", err)
 	}
 
-	srv := server.New(*addr, absLibrary, imp, w.Scan)
+	srv := server.New(addr, absLibrary, imp, w.Scan)
 
 	go w.Start()
 	go srv.Start()
 
-	log.Printf("mnemosyne started: watching %s, library at %s, web at %s", *importDir, *libraryDir, *addr)
+	log.Printf("mnemosyne started: watching %s, library at %s, web at %s", importDir, absLibrary, addr)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
@@ -50,4 +44,31 @@ func main() {
 	log.Println("shutting down...")
 	w.Stop()
 	srv.Stop()
+}
+
+func envRequired(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		log.Fatalf("required environment variable %s is not set", key)
+	}
+	return val
+}
+
+func envDefault(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
+
+func envDefaultInt(key string, fallback int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return fallback
+	}
+	n, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatalf("environment variable %s must be an integer: %v", key, err)
+	}
+	return n
 }
