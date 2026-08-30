31a58651746a5963be889838bf2b2ca1b1b3d441
kfang <kfang@users.noreply.github.com>
2026-08-30 07:59:02 -0700

    bump

--- whisper-srt/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MOVIE='/mnt/f/Yuru Camp Live Action 1080p S2 10bit/[Shiniori-Raws] Yuru Camp 2 Live Action - 00 (BD 1920x1080 x264 10bit FLAC).mkv'
-
-MODEL='medium'
-LANGUAGE='Japanese'
-
-./whisper-cli "$MOVIE" --model $MODEL --language $LANGUAGE --task transcribe --output_format srt
