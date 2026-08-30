b4b9784f3aeec28a7ca254f42db49c3858680815
kfang <kfang@users.noreply.github.com>
2026-08-30 08:28:16 -0700

    learning some rust

--- toshobook/src/main.rs
+++ toshobook/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Name of the file to open
+    #[arg(short, long)]
+    file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("file: {}", args.file.expect("file not declared"));
 }
