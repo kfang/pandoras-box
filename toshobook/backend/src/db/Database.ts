86417c251c6ae2e6023377bcc9c3f458c2715ba1
kfang <kfang@users.noreply.github.com>
2026-09-15 19:51:28 -0700

    refactor migrations

--- toshobook/backend/src/db/Database.ts
+++ toshobook/backend/src/db/Database.ts
@@ -1,5 +1,5 @@
 import SQLite from "better-sqlite3";
-import { Console, Context, Effect, FileSystem, Layer, Path } from "effect";
+import { Console, Context, Data, Effect, FileSystem, Layer, Path } from "effect";
 import { Kysely, SqliteDialect, type Generated } from "kysely";
 import { FileMigrationProvider, Migrator } from "kysely/migration";
 
@@ -23,41 +23,58 @@ export class DatabaseService extends Context.Service<
   }
 >()("DatabaseService") { }
 
+export class DatabaseMigrationFailure extends Data.TaggedError("DatabaseMigrationFailure")<{
+  readonly message: string;
+  readonly cause: unknown;
+}> { }
+
+const migrationResult = (db: Kysely<Database>) => Effect.gen(function* () {
+  const log = yield* Console.Console;
+  const filesys = yield* FileSystem.FileSystem;
+  const path = yield* Path.Path;
+
+  const provider = new FileMigrationProvider({
+    fs: { readdir: (path) => Effect.runPromise(filesys.readDirectory(path)) },
+    path: { join: path.join },
+    migrationFolder: path.join(import.meta.dirname, "migrations"),
+  });
+
+  const migrator = new Migrator({ db, provider });
+
+  log.info("running database migrations")
+  const migrateResult = yield* Effect.tryPromise(() => migrator.migrateToLatest());
+
+  migrateResult.results?.forEach((res) => {
+    log.debug(`...name=${res.migrationName} direction=${res.direction} status=${res.status}`);
+  });
+
+  if (migrateResult.error) {
+    return yield* Effect.die(new DatabaseMigrationFailure({
+      message: "database migration failed",
+      cause: migrateResult.error,
+    }))
+  }
+
+  return migrateResult;
+});
+
 export const DatabaseServiceLive = Layer.effect(
   DatabaseService,
   Effect.gen(function* () {
     const db = yield* Effect.acquireRelease(
       Effect.gen(function* () {
-        const log = yield* Console.Console;
-        const filesys = yield* FileSystem.FileSystem;
-        const path = yield* Path.Path;
-
         // TODO: grab configuration
-        const dialect = new SqliteDialect({
-          database: new SQLite(":memory:"),
-        });
+        const database = new SQLite(":memory:");
+        const dialect = new SqliteDialect({ database });
         const db = new Kysely<Database>({ dialect });
 
-        log.info("...running migrations (TODO)")
-        const readdir = (path: string): Promise<string[]> => Effect.runPromise(filesys.readDirectory(path));
-        const join = path.join;
-        const provider = new FileMigrationProvider({
-          fs: { readdir },
-          path: { join },
-          migrationFolder: path.join(import.meta.dirname, "migrations"),
-        });
-        const migrator = new Migrator({ db, provider });
-        const migrateResult = yield* Effect.tryPromise(() => migrator.migrateToLatest());
-
-        migrateResult.results?.forEach((res) => {
-          log.info(`${res.direction}\t${res.status}\t${res.migrationName}`);
-        })
+        // run through the database migrations
+        yield* migrationResult(db);
 
         return db;
       }),
-      (db) => Effect.gen(function* () {
-        yield* Effect.promise(db.destroy);
-      }),
+      // on release, destroy db
+      (db: Kysely<Database>) => Effect.promise(db.destroy),
     );
     return { db };
   }),
